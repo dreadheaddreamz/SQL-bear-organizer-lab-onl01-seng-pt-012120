971ea71c42126d6dd2d874eec419d0dad7452780
James Batsell <jamesbatsell05@gmail.com>
2020-04-04 23:49:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
- INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr.Chocolate", 12, "male", "black", "calm", 1);
+ INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+ VALUES (1, "Mr.Chocolate", 12, "male", "black", "calm", 1);
